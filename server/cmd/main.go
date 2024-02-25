c390b1a14e1385b1170aa0ccc68f0bd76d1477aa
Сабина <sabinazholobova@gmail.com>
2024-02-25 10:50:31 +0300

    Main.go refactoring

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"log"
 	"server/handler"
 	"server/repository"
@@ -14,7 +15,7 @@ func main() {
 		Port:     "5432",
 		User:     "postgres15",
 		Password: "password",
-		DBName:   "online-store",
+		DBName:   "online-store-db",
 		SSLMode:  "disable",
 	}
 	db, err := repository.NewDatabase(dbConf)
@@ -23,13 +24,14 @@ func main() {
 		log.Fatalf("Error: unable to connect to database")
 	}
 
-	userRep := repository.NewUserRepository(db.GetDB())
-	productRep := repository.NewProductRepository(db.GetDB())
-	userService := service.NewUserService(userRep)
-	productService := service.NewProductService(productRep)
-	userHandler := handler.NewUserHandler(userService)
-	productHandler := handler.NewProductHandler(productService)
+	rep := repository.NewRepository(db.GetDB())
+	serv := service.NewService(rep)
+	hand := handler.NewHandler(serv)
 
-	r := router.InitRouter(userHandler, productHandler)
-	router.Start(r, "0.0.0.0:8080")
+	r := router.InitRouter(hand)
+
+	err = router.Start(r, "0.0.0.0:8080")
+	if err != nil {
+		logrus.Fatal("Cant run")
+	}
 }
