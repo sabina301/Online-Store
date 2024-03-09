964c49394025181c74fe7c34613fe6ea10b2c724
sabina301 <sabinazholobova@gmail.com>
2024-03-09 22:19:10 +0300

    Delete bug with middleware

--- server/handler/middleware.go
+++ server/handler/middleware.go
@@ -12,23 +12,29 @@ func (h *Handler) UserIdentity(c *gin.Context) {
 	header := c.GetHeader("Authorization")
 	cookie, _ := c.Cookie("token")
 	if header == "" && cookie == "" {
-		response.NewError(c, "Empty auth header or httpOnlyCookie", http.StatusUnauthorized)
+		response.NewError(c, "empty auth header name or httpOnlyCookie", http.StatusUnauthorized)
 		return
 	}
 	var token string
 	if header != "" {
 		headerParts := strings.Split(header, " ")
-		if len(headerParts) != 2 {
-			response.NewError(c, "Invalid auth header", http.StatusUnauthorized)
+
+		if len(headerParts) != 2 || headerParts[1] == " " || headerParts[1] == "" {
+			response.NewError(c, "token is empty", http.StatusUnauthorized)
+			return
+		}
+		if headerParts[0] != "Bearer" {
+			response.NewError(c, "invalid header value", http.StatusUnauthorized)
 			return
 		}
 		token = headerParts[1]
 	} else {
 		token = cookie
 	}
+
 	userId, userRole, err := h.serv.ParseToken(token)
 	if err != nil {
-		response.NewError(c, "Invalid token", http.StatusUnauthorized)
+		response.NewError(c, "invalid token", http.StatusUnauthorized)
 		return
 	}
 	c.Set("userId", userId)
