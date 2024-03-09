12735a8d3f914e81dfa9a8357a39daff6af7d27e
sabina301 <sabinazholobova@gmail.com>
2024-03-09 22:15:20 +0300

    Add a good server shutdown

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"log"
 	"os"
+	"os/signal"
 	"server/handler"
 	"server/repository"
 	"server/router"
 	"server/service"
+	"syscall"
 )
 
 func main() {
@@ -48,11 +51,24 @@ func main() {
 	if err != nil {
 		logrus.Println("Cant create admin")
 	}
-	err = srv.Start(r, viper.GetString("port"))
-	if err != nil {
-		logrus.Fatal("Cant run")
-	}
+	go func() {
+		err = srv.Start(r, viper.GetString("port"))
+		if err != nil {
+			logrus.Fatal("Cant run")
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
 
+	err = srv.Finish(context.Background())
+	if err != nil {
+		logrus.Error(err.Error())
+	}
+	err = db.CloseDB()
+	if err != nil {
+		logrus.Error(err.Error())
+	}
 }
 
 func initConfig() error {
