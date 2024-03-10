89024f8cf9e955ab2820413ddaafc718673d1be3
sabina301 <sabinazholobova@gmail.com>
2024-03-10 16:04:43 +0300

    Add products table

--- server/db_description/init.sql
+++ server/db_description/init.sql
@@ -3,5 +3,14 @@ CREATE TABLE users(
     username VARCHAR(30) NOT NULL UNIQUE ,
     password_hash VARCHAR(150) NOT NULL,
     role VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    category VARCHAR(30) NOT NULL ,
+    name VARCHAR(30) NOT NULL ,
+    color VARCHAR(30),
+    description VARCHAR(200) ,
+    price INT NOT NULL
 )
 
