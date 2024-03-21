87dd62326081c643d2713140305d3f6279948112
sabina301 <sabinazholobova@gmail.com>
2024-03-21 11:05:44 +0300

    Change admin catalog

--- server/handler/admin_catalog_handler.go
+++ server/handler/admin_catalog_handler.go
@@ -11,12 +11,12 @@ func (h *Handler) AddProduct(c *gin.Context) {
 	var input entity.Product
 	err := c.BindJSON(&input)
 	if err != nil {
-		response.NewError(c, err.Error(), http.StatusBadRequest)
+		response.NewError(c, "Invalid input", http.StatusBadRequest)
 		return
 	}
 	id, err := h.serv.AddProduct(input)
 	if err != nil {
-		response.NewError(c, err.Error(), http.StatusBadRequest)
+		response.NewError(c, "Invalid data", http.StatusBadRequest)
 		return
 	}
 	c.JSON(http.StatusOK, map[string]interface{}{
